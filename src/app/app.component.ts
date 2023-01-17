f6677cceeaf531a3bab235ac8d47f063987ab08d
Martin Casado <bomblikeapps@gmail.com>
2023-01-17 02:51:22 -0300

    logic & style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +7,15 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('clp')
-  clp!: HTMLInputElement;
-  @ViewChild('usd')
-  usd!: HTMLInputElement;
-  @ViewChild('ars')
-  ars!: HTMLInputElement;
+  blueForm: FormGroup = new FormGroup({
+    clp: new FormControl(''),
+    usd: new FormControl(''),
+    ars: new FormControl(''),
+  });
   title = 'Blue Chile';
   blueRate: string = '';
   clpRate: string = '';
+  
   async ngOnInit(): Promise<void> {
     await this.getRates();
   }
@@ -45,6 +46,41 @@ export class AppComponent implements OnInit {
   }
 
   convert(e: any) {
-    this.usd.innerHTML = (+e.target.value * +this.clpRate).toString();
+    let targetId = '';
+    if (!e) {
+      if (this.blueForm.get('clp')?.value && this.blueForm.get('clp')?.value > 0) {
+        targetId = 'clp';
+      } else if (this.blueForm.get('usd')?.value && this.blueForm.get('usd')?.value > 0) {
+        targetId = 'usd';
+      } else if (this.blueForm.get('ars')?.value && this.blueForm.get('ars')?.value > 0) {
+        targetId = 'ars';
+      }
+    }
+    else {
+      targetId = e.target.id;
+    }
+    switch(targetId) {
+      case 'clp':
+        this.blueForm.patchValue({ 
+          usd: (this.blueForm.get('clp')?.value / +this.clpRate).toFixed(2),
+          ars: ((this.blueForm.get('clp')?.value / +this.clpRate) * +this.blueRate).toFixed(2),
+        });
+        break;
+      case 'usd':
+        this.blueForm.patchValue({ 
+          clp: (this.blueForm.get('usd')?.value * +this.clpRate).toFixed(2),
+          ars: (this.blueForm.get('usd')?.value * +this.blueRate).toFixed(2),
+        });
+        break;
+      case 'ars':
+        this.blueForm.patchValue({ 
+          clp: ((this.blueForm.get('ars')?.value / +this.blueRate) * +this.clpRate).toFixed(2),
+          usd: (this.blueForm.get('ars')?.value / +this.blueRate).toFixed(2),
+        });
+        break;
+      default:
+        this.blueForm.reset();
+        break;
+    }
   }
 }
